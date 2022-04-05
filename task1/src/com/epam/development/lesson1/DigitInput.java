5628a0a24702bc66431cdb948f7d6072023de6d2
Сергей Пасиченко <50148048+loki911@users.noreply.github.com>
2022-04-05 14:33:42 +0300

    Update DigitInput.java

--- task1/src/com/epam/development/lesson1/DigitInput.java
+++ task1/src/com/epam/development/lesson1/DigitInput.java
@@ -1,13 +1,32 @@
 package com.epam.development.lesson1;
 
 import java.util.Scanner;
+import java.util.regex.*;
 
 public class DigitInput {
 
     public int mainNumber() {
-        @SuppressWarnings("resource")
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите четырехзначное положительное число: ");
-        return in.nextInt();
+        boolean isInputCorrect = false;
+        int output = 0;
+        Pattern numberPattern = Pattern.compile("\\d{4}");
+        System.out.println("Введите четырехзначное положительное число:");
+
+        while (!isInputCorrect) {
+            @SuppressWarnings("resource")
+            Scanner in = new Scanner(System.in);
+            String userInput = in.next();
+
+            Matcher matcher = numberPattern.matcher(userInput);
+            isInputCorrect =  matcher.matches();
+
+            if (isInputCorrect) {
+                output = Integer.parseInt(userInput);
+            }
+            else {
+                System.out.println("Введенное значение некорректно. Введите четырехзначное положительное число:");
+            }
+        }
+
+        return output;
     }
 }
